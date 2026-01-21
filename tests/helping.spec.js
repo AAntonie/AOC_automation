e958db0c1ddb9268fa2bc2a20282f11f3c730ea9
Ana Antonie <ana.antonie19@gmail.com>
2026-01-21 10:58:43 +0200

    Update helping.spec.js

--- tests/helping.spec.js
+++ tests/helping.spec.js
@@ -1,29 +1,29 @@
-import { test, expect } from '@playwright/test';
-import { HelpingPage } from '../../pages/helping'
+import { test, expect } from "@playwright/test";
+import { HelpingPage } from "../pages/helping";
 
-test('Caută joburi și apasă săgețile', async ({ page }) => {
+test("Caută joburi și apasă săgețile", async ({ page }) => {
   const helping = new HelpingPage(page);
 
   await helping.gotoHelpingPage();
 
-  await helping.searchJobs([
-    'Tester',
-    'Developer',
-    'Automation'
-  ]);
-
+  await helping.searchJobs(["Tester", "Developer", "Automation"]);
 });
 
-
-test('Link email este clickable', async ({ page }) => {
+test("Link email este clickable", async ({ page }) => {
   const helping = new HelpingPage(page);
   await helping.gotoHelpingPage();
   await helping.clickEmail();
-  await expect(helping.emailLink).toHaveAttribute('href', 'mailto: aocpeviitor@gmail.com');
+  await expect(helping.emailLink).toHaveAttribute(
+    "href",
+    "mailto: aocpeviitor@gmail.com",
+  );
 });
-test('Link Declaratie este clickable', async ({ page }) => {
+test("Link Declaratie este clickable", async ({ page }) => {
   const helping = new HelpingPage(page);
   await helping.gotoHelpingPage();
   await helping.clickDeclaratie();
-  await expect(helping.declaratieLink).toHaveAttribute('href', 'https://redirectioneaza.ro/asociatia-oportunitati-si-cariere/');
+  await expect(helping.declaratieLink).toHaveAttribute(
+    "href",
+    "https://redirectioneaza.ro/asociatia-oportunitati-si-cariere/",
+  );
 });
